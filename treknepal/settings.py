62029a2000067de54f252c9617910b72c7cda641
Subash Ghimire <ghimiresubash980843@gmail.com>
2025-06-14 10:10:27 +0545

    requirements created

--- treknepal/settings.py
+++ treknepal/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1fm0(4npxz125^gfr5_vl^os6!#!14i4#%14g9fx4jrzc159pq'
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-key")
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
