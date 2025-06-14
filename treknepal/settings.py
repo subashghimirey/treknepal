cc8022af6d98dd1f4d0764f1559d6fc6b95cf181
Subash Ghimire <ghimiresubash980843@gmail.com>
2025-06-14 17:27:07 +0545

    cors headers update

--- treknepal/settings.py
+++ treknepal/settings.py
@@ -57,9 +57,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
+    
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
